7a95d68d67e8e12e593f7a8b255c63cc22f6a0c2
molexandev01~ <molexandev@gmail.com>
2023-09-21 18:46:17 +0300

    I continue to work on the project

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,45 @@
-$(function () {
-
-});
-
+$(function () {});
 
 const isMobile = {
-   Android: function () {
-       return navigator.userAgent.match(/Android/i);
-   },
-   BlackBerry: function () {
-       return navigator.userAgent.match(/BlackBerry/i);
-   },
-   iOS: function () {
-       return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-   },
-   Opera: function () {
-       return navigator.userAgent.match(/Opera Mini/i);
-   },
-   Windows: function () {
-       return navigator.userAgent.match(/IEMobile/i);
-   },
-   any: function () {
-       return (
-           isMobile.Android() ||
-           isMobile.BlackBerry() ||
-           isMobile.iOS() ||
-           isMobile.Opera() ||
-           isMobile.Windows()
-       );
-   }
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    },
 };
 
 if (isMobile.any()) {
-  document.body.classList.add('__touch');
-}else{
-     document.body.classList.add('__pc');
+    document.body.classList.add("__touch");
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if(menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", function (e) {
+                menuArrow.parentElement.classList.toggle("__active")
+            });
+        }
+    }
+
+} else {
+    document.body.classList.add("__pc");
 };
