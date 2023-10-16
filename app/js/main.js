9a0ea7756430a409863d20589b5389321abf076a
molexandev01~ <molexandev@gmail.com>
2023-10-16 20:09:39 +0300

    added category page & product page

--- app/js/main.js
+++ app/js/main.js
@@ -70,8 +70,13 @@ const menuBtn = document.querySelector('.menu__btn')
      $(".reviews__rating").rateYo({
         rating: 5,
         ratedFill: "#FFA858",
-        // starSvg: '<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.60677 1.09367C6.91437 0.189974 8.19249 0.189977 8.50009 1.09367L9.48362 3.98313C9.62156 4.3884 10.0022 4.66091 10.4303 4.66091H13.5105C14.4904 4.66091 14.8856 5.92416 14.0804 6.48263L11.6696 8.15456C11.3028 8.40901 11.149 8.87584 11.2929 9.29851L12.2324 12.0586C12.5432 12.9718 11.5085 13.7523 10.7158 13.2025L8.12333 11.4046C7.78058 11.1668 7.32629 11.1668 6.98354 11.4046L4.39107 13.2025C3.5984 13.7523 2.56367 12.9718 2.87451 12.0586L3.81399 9.29851C3.95786 8.87584 3.8041 8.40901 3.43722 8.15456L1.02648 6.48263C0.221246 5.92416 0.616433 4.66091 1.59638 4.66091H4.67659C5.10469 4.66091 5.4853 4.3884 5.62325 3.98313L6.60677 1.09367Z" fill="#FFA858"/></svg>',
-        // normalFill: "transparent",
+        starWidth: "15px",
+        spacing: "3px",
+     });
+
+     $(".product-top__rating").rateYo({
+        rating: 5,
+        ratedFill: "#FFA858",
         starWidth: "15px",
         spacing: "3px",
      });
@@ -88,4 +93,6 @@ const menuBtn = document.querySelector('.menu__btn')
     });
 
 
-           
+    // FORM STYLLER
+
+    $('.product-top__input').styler();
