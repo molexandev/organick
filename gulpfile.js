1adc96d12f7ae36a6d4bdf70a95ca792515ae811
molexandev01~ <molexandev@gmail.com>
2023-10-18 21:39:15 +0300

    added several pages

--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,7 @@ function scripts() {
       'node_modules/jquery/dist/jquery.js',
       'node_modules/rateyo/src/jquery.rateyo.js',
       'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
       'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
       'app/js/main.js'
    ])
