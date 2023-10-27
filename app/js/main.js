8454e0fc4775fc1af94b912c53d4b2f1ed7387fd
molexandev01~ <molexandev@gmail.com>
2023-10-27 20:16:48 +0300

    added adaptive to ABOUT US page

--- app/js/main.js
+++ app/js/main.js
@@ -101,3 +101,15 @@ const menuBtn = document.querySelector('.menu__btn')
     // FORM STYLLER
 
     $('.product-top__input').styler();
+
+
+    // WOW
+
+    // wow = new WOW({
+    //     boxClass: "wow",
+    //     animateClass: "animate__animated",
+    //     offset: 0,
+    //     mobile: true,
+    //     live: true,
+    // });
+    // wow.init();
